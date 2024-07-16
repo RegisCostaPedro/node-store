de3438ca0ddd130010057fb993aa3e44fc09b99b
regis <regiscostapedro0705@gmail.com>
2024-07-16 13:36:13 -0300

    Feat: Criando serviço de autenticação

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = '5fbee1a43e26a53a1c62a7f1';
+global.EMAIL_TMPL = '<strong>{0}</strong>' //template email
+
+module.exports ={
+    connectionString : 'mongodb://localhost:27017/node-store',
+    sendgridKey: 'TBD',
+    containerConnectionString: 'TBD'
+}
